7b0f862efee23236cb0e4bda4b1239d76c5a5bea
José Antonio Llamas Álvarez <jallamasalvarez@gmail.com>
2018-12-12 12:37:02 +0100

    Creada clase model.Jugador

---
+++ src/model/Jugador.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Jugador {
+
+	private String nombre;
+	private int puntuacion, comodines;
+	private boolean eleccion;//True si ha elegido pregunta y false si es reto
+
+	public Jugador(String nombre, int puntuacion, int comodines, boolean eleccion) {
+		this.nombre = nombre;
+		this.puntuacion = puntuacion;
+		this.comodines = comodines;
+		this.eleccion = eleccion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+	public int getComodines() {
+		return comodines;
+	}
+
+	public void setComodines(int comodines) {
+		this.comodines = comodines;
+	}
+
+	public boolean isEleccion() {
+		return eleccion;
+	}
+
+	public void setEleccion(boolean eleccion) {
+		this.eleccion = eleccion;
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + nombre + ", puntuacion=" + puntuacion + ", comodines=" + comodines + ", eleccion="
+				+ eleccion + "]";
+	}
+	
+	
+}
