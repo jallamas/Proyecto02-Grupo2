c3afad4bc89d967823fd404151d1feac7387d384
Ricardo Mejías <mejiasdoradoricardo@gmail.com>
2018-12-13 10:56:16 +0100

    En este commit se añaden vistas

---
+++ src/vista/ImprimirCreditos.java
@@ -0,0 +1,11 @@
+package vista;
+
+public class ImprimirCreditos {
+
+	//TODO  Hacer impresion de los nombres del grupo
+	public static void imprimirCreditos() {
+		
+		
+	}
+	
+}
---
+++ src/vista/Test.java
@@ -0,0 +1,12 @@
+package vista;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ImprimirVictoria.imprimirVictoria();
+//		ImprimirIntrucciones.imprimirInstrucciones();
+
+	}
+
+}
