60adab98bdc78a491ea2d0312f805f3fb5ffb8f8
Alberto Santiago Rodríguez <albertosantiagorodriguez@gmail.com>
2018-12-13 11:53:13 +0100

    Creación crudPartida Creación método crearPartida

--- src/crud/CrudPartida.java
+++ src/crud/CrudPartida.java
@@ -1,5 +1,20 @@
 package crud;
 
-public class CrudPartida {
+import model.Jugador;
+import model.Partida;
 
+public class CrudPartida {
+	
+	public Partida crearPartida(int numJug, int ptosVict, double probabilidadComodin,double probabilidadRobarComodin, Jugador[] jugadores) {
+		int maxRondas=0;
+		Partida p;
+			if (ptosVict<=20) {
+				maxRondas=ptosVict + 2;
+			}else if(ptosVict>20){
+				maxRondas=ptosVict+(ptosVict*20/100);
+			}
+		p= new Partida(numJug,ptosVict,maxRondas, probabilidadComodin, probabilidadRobarComodin, jugadores);
+	return p;
+	}
+	
 }
