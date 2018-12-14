874f7d3ae79efc6f300bfcfce58ddca5653d8050
Víctor Miranda Alcántara <victor.miranda.alcantara12@gmail.com>
2018-12-14 13:43:52 +0100

    AÑADIDO DATOS PREGUNTAS Y RESPUESTAS

--- src/datos/Enunciados.java
+++ src/datos/Enunciados.java
@@ -1,5 +1,52 @@
 package datos;
 
+import java.util.Arrays;
+
 public class Enunciados {
 
+	String enunciado;
+	int tipo;
+	Respuestas lista[];
+	
+	public Enunciados () {
+		
+	}
+
+	public Enunciados(String enunciado, int tipo, Respuestas[] lista) {
+		super();
+		this.enunciado = enunciado;
+		this.tipo = tipo;
+		this.lista = lista;
+	}
+
+	public String getEnunciado() {
+		return enunciado;
+	}
+
+	public void setEnunciado(String enunciado) {
+		this.enunciado = enunciado;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public Respuestas[] getLista() {
+		return lista;
+	}
+
+	public void setLista(Respuestas[] lista) {
+		this.lista = lista;
+	}
+
+	@Override
+	public String toString() {
+		return "Enunciados [enunciado=" + enunciado + ", tipo=" + tipo + ", lista=" + Arrays.toString(lista) + "]";
+	}
+
+	
 }
--- src/datos/Respuestas.java
+++ src/datos/Respuestas.java
@@ -1,5 +1,38 @@
 package datos;
 
 public class Respuestas {
+	//atributos
+	String texto;
+	int verdaderoFalso;
+	
+	public Respuestas() {
+		
+	}
+	public Respuestas(String texto, int verdaderoFalso) {
+		super();
+		this.texto = texto;
+		this.verdaderoFalso = verdaderoFalso;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public int getVerdaderoFalso() {
+		return verdaderoFalso;
+	}
+	public void setVerdaderoFalso(int verdaderoFalso) {
+		this.verdaderoFalso = verdaderoFalso;
+	}
+	@Override
+	public String toString() {
+		return "Respuestas [texto=" + texto + ", verdaderoFalso=" + verdaderoFalso + "]";
+	}
+	
+	
+	
+	
+	
 
 }
