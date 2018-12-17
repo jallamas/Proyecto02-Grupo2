38a174bfc58cfd5abd9091328ed8826b566f96e0
José Antonio Llamas Álvarez <jallamasalvarez@gmail.com>
2018-12-17 14:08:40 +0100

    cambios

---
+++ src/crud/Crudpartida.java
@@ -0,0 +1,19 @@
+package crud;
+
+import model.Jugador;
+import model.Partida;
+
+public class Crudpartida {
+
+	public Partida crearPartida(int numJug, int ptosVict, double probabilidadComodin, double probabilidadRobarComodin,
+			Jugador[] jugadores) {
+		int maxRondas = 0;
+		if (ptosVict <= 20) {
+			maxRondas = ptosVict + 2;
+		} else if (ptosVict > 20) {
+			maxRondas = ptosVict + (ptosVict * 20 / 100);
+		}
+		Partida p = new Partida(numJug, ptosVict, maxRondas, probabilidadComodin, probabilidadRobarComodin, jugadores, null);
+		return p;
+	}
+}
