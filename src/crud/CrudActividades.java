474ca51d2006da1e892f1847f99536fbfa84dc25
José Antonio Llamas Álvarez <jallamasalvarez@gmail.com>
2018-12-17 11:59:21 +0100

    Eliminadas las clases model.Enunciado y vista.Maindeprueba

--- src/crud/CrudActividades.java
+++ src/crud/CrudActividades.java
@@ -1,5 +1,11 @@
 package crud;
+import model.Actividades;
 
 public class CrudActividades {
 
+	// Método para mostrar una pregunta con sus respuestas
+	
+	public void imprimirPregunta() {
+		
+	}
 }
