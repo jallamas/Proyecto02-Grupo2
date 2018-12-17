af739f74d6a953db00c7e688c790d6bfd542769a
Ricardo <mejiasdoradoricardo@gmail.com>
2018-12-17 18:02:38 +0100

    Añadido metodo para imprimir preguntas y respuestas

--- src/crud/CrudActividades.java
+++ src/crud/CrudActividades.java
@@ -1,11 +1,16 @@
 package crud;
+import java.util.Random;
+
 import model.Actividades;
 
 public class CrudActividades {
 
 	// Método para mostrar una pregunta con sus respuestas
 	
-	public void imprimirPregunta() {
-		
+	public void imprimirPregunta(Actividades[] a) {
+		Random r = new Random();
+		int preguntaAleatoria = r.nextInt(a.length-1+1)-1;
+		System.out.println(a[preguntaAleatoria].getEnunciado());
+		System.out.println(a[preguntaAleatoria].getRespuestas());
 	}
 }
--- src/crud/Crudpartida.java
+++ src/crud/Crudpartida.java
@@ -2,6 +2,7 @@ package crud;
 
 import model.Jugador;
 import model.Partida;
+import utilidades.Probabilidad;
 
 public class Crudpartida {
 
@@ -13,7 +14,38 @@ public class Crudpartida {
 		} else if (ptosVict > 20) {
 			maxRondas = ptosVict + (ptosVict * 20 / 100);
 		}
-		Partida p = new Partida(numJug, ptosVict, maxRondas, probabilidadComodin, probabilidadRobarComodin, jugadores, null);
+		Partida p = new Partida(numJug, ptosVict, maxRondas, probabilidadComodin, probabilidadRobarComodin, jugadores,
+				null);
 		return p;
+	}
+
+	public void generarEvento() {
+		float probabilidad = 0.70F;
+		int numeroProbable = 3;
+		int opciones = 3;
+		int res = Probabilidad.generarProbabilidad(opciones, numeroProbable, probabilidad);
+
+		if (res == 1) {
+
+			// darComodin();
+		}
+
+		else if (res == 2) {
+
+			// Robar comodin
+		}
+
+	}
+	// public void darComodin(Jugador j1) {
+	//
+	// int comodines = j1.getComodinesactuales();
+	// comodines = comodines++;
+	//
+	// }
+
+	public void quitarComodin(Jugador l1[], int opcion) {
+		for (int i = 0; i < l1.length; i++) {
+			System.out.println(i++ + "-" + l1[i].getNombre());
+		}
 	}
 }
