c04ce6b874c43ea07deef7013e29d0e0281938c5
Ricardo <mejiasdoradoricardo@gmail.com>
2019-01-10 16:18:22 +0100

    Añadido Robar comodin e impresiones por pantalla dentro del metodo generarEvento

--- src/controller/controllerPartida.java
+++ src/controller/controllerPartida.java
@@ -3,16 +3,23 @@ package controller;
 import java.util.Random;
 import vista.ImprimirJugadores;
 import model.Jugador;
+import utilidades.Leer;
+import vista.ImprimirComodinObtenido;
+import vista.ImprimirRobo;
 
 public class controllerPartida {
 
-	public void generarEvento(Jugador j1, int probmascomodin, int probrobarcomodin) {
+	public void generarEvento(Jugador j1, Jugador j2[], int probmascomodin, int probrobarcomodin)
+			throws InterruptedException {
 		Random r = new Random();
 		int aleatorio = r.nextInt(100 - 1) + 1;
 		if (aleatorio <= probmascomodin) {
-			// darComodin();
+			ImprimirComodinObtenido.imprimirObtenerComodin();
+			darComodin(j1);
 		} else if (aleatorio > probmascomodin && aleatorio <= probrobarcomodin) {
-			// RobarComodin();
+			ImprimirRobo.imprimirRobo();
+			darComodin(j1);
+			robarComodin(j2);
 		}
 
 	}
@@ -22,11 +29,19 @@ public class controllerPartida {
 		j1.setComodines(j1.getComodines() + uno);
 	}
 
-	public static void robarComodin(Jugador j1[], int opcion) {
-		ImprimirJugadores.ImprimirJugador(j1);
-		int res = j1[opcion - 1].getComodines();
-		res = res--;
-
+	public static void robarComodin(Jugador j2[]) {
+		int opcion;
+		int uno = 1;
+		ImprimirJugadores.ImprimirJugador(j2);
+		do {
+			System.out.println("Elija una opción");
+			opcion = Leer.datoInt();
+			if (j2[opcion - uno].getComodines() != 0) {
+				j2[opcion - 1].setComodines(j2[opcion - uno].getComodines() - uno);
+			} else {
+				System.out.println("Este jugador no tiene comodines, elije otro");
+			}
+		} while (j2[opcion - 1].getComodines() == 0);
 	}
 
 	public void limpiarPantalla() {
